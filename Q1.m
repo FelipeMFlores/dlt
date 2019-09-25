82138eb835877fbeb0c8323011064a6784faa029
FelipeMFlores <felipe.melloflores@gmail.com>
2019-09-25 18:19:21 -0300

    dlt and Q1 by julia

---
+++ Q1.m
@@ -0,0 +1,37 @@
+I = imread('maracana1.jpg');
+
+x = [ 0 0 0 1; 7.32 0 0 1; 0 0 2.44 1; 7.32 0 2.44 1;
+      -5.5 0 0 1; -5.5 -5.5 0 1; 12.82 0 0 1; 12.82 -5.5 0 1;
+      23.82 0 0 1; -16.5 0 0 1; -16.5 -16.5 0 1];
+y = [ 125 158 1; 160 140 1; 125 127 1; 160 111 1;
+      97 172 1; 160 176 1; 184 128 1; 241 132 1;
+      230 106 1; 32 205 1; 250 222 1];
+
+P = dlt(x,y);
+Pinv = [P(1,1) P(1,2) P(1,4);
+        P(2,1) P(2,2) P(2,4);
+        P(3,1) P(3,2) P(3,4)];
+
+while true    
+    imshow(I);
+    fpos_img = [ginput(1) 1]';
+
+    fpos_w2d = Pinv\fpos_img;
+    fpos_w2d = fpos_w2d/fpos_w2d(3,1);
+    fpos_w3d = [fpos_w2d(1,1); fpos_w2d(2,1); 0; 1];
+    hpos_w3d = [fpos_w3d(1,1); fpos_w3d(2,1); 1.8; 1];
+
+    hpos_img = P*hpos_w3d;
+    hpos_img = hpos_img/hpos_img(3,1);
+    hpos_img = round(hpos_img);
+
+    I(hpos_img(2,1), hpos_img(1,1),:) = 255;
+    I(fpos_img(2,1), fpos_img(1,1),:) = 255;
+    
+    h = round((hpos_img(1,1)+fpos_img(1,1))/2);
+    for i=hpos_img(2,1):fpos_img(2,1)
+        I(i,h,:) = 255;
+    end
+    
+    imshow(I);
+end
--- dlt.m
+++ dlt.m
@@ -1,49 +1,24 @@
-function A = dlt(x,y)
-% A = DLT(x,y) solves for the projective transformation matrix A with respect to
-% the linear system y ~ Ax where ~ denotes equality up to a scale, using the
-% Direct Linear Transformation technique. A is a m-by-n matrix, x is a n-by-k
-% matrix that contains k source points in column vector form and y is a m-by-k
-% matrix containning k target points in column vector form. The solution is
-% normalised as any multiple of A also satisfies the equation.
-%
-% A = dlt(x,y,dim) further specifies the vanishing dimension 0 < dim <= m, by
-% which the remaining rows in y are divided. By default dim is set to m, the
-% last row of y. The vanashing dimension has to be chosen carefully to avoid
-% the singularity of division by zero.
-%
-% Example:
-%
-%   A = rand(3,4);
-%   x = rand(4,12);
-%   A = A / norm(A);
-%   A - dlt(x,A*x) % should be a small number
-%
-% By Hsiang-Jen (Johnny) Chien (jchien@aut.ac.nz) from Centre of Robotics and
-% Vision (CeRV), Auckland University of Technology, New Zealand.
-%
-[n,k ] = size(x);
-[m,ky] = size(y);
-q = m;
-% dimensionality check
-assert(k == ky);
-assert(q > 0 && q <= m);
-% make y inhomogeneous
-r = setdiff(1:m,q);
-y = bsxfun(@rdivide,y(r,:),y(q,:));
-% build the homogeneous linear system
-A = zeros(m,n,k,m-1);
-for i = 1:numel(r)
-    j = r(i);
-    b = -bsxfun(@times,x,y(i,:));
-    A(j,:,:,i) = reshape(x,1,n,k);
-    A(q,:,:,i) = reshape(b,1,n,k);
+function P = dlt(x, y)
+% x = pontos no mundo 3d (k linhas e m colunas)
+% y = pontos correspondentes na imagem (k linhas e n colunas)
+
+m = size(x, 2);
+n = size(y, 2);
+k = size(x, 1);
+
+h = 1;
+A = zeros(2*k, 12);
+for i = 1:k
+    A(h,:) = [  x(i,1) x(i,2) x(i,3) 1 0 0 0 0 ...
+                -y(i,1)*x(i,1) -y(i,1)*x(i,2) -y(i,1)*x(i,3) -y(i,1)    ];
+    A(h+1,:) = [0 0 0 0 x(i,1) x(i,2) x(i,3) 1 ...
+                -y(i,2)*x(i,1) -y(i,2)*x(i,2) -y(i,2)*x(i,3) -y(i,2)    ];
+            
+    h = h+2;
 end
-% convert to a big flat matrix
-A = reshape(A,m*n,[]);
-% solve the homogeneous linear system using SVD
-[~,~,V] = svd(A*A');
-% the solution minimising |A'A| is the right singular vector
-% corresponding to the smallest singular value
-A = reshape(V(:,end),m,n);
-% some normalisation, optional
-A = A / norm(A) * sign(A(1,1));
+
+[~, ~, V] = svd(A'*A);
+Pt = reshape(V(:,end),m,n);
+P = Pt';
+
+end
